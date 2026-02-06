70c28c82ed32e8299bc862bd2b09a2339768a4dd
mkrafiul13453 <mkrafiul13453@gamil.com>
2026-02-06 22:00:55 +0600

    problem-2 is done

--- problem-2.js
+++ problem-2.js
@@ -1,1 +1,27 @@
-console.log(typeof(4));
+function validOtp(otp) {
+    const otpLength=otp.length;
+
+    if(typeof otp !=="string"){
+        return "Invalid";
+    }
+
+    else if(otpLength<8){
+        return false;
+    }
+
+   
+
+    else if(!otp.startsWith("ph-")){
+        return false;
+    }
+
+    else{
+        return true;
+    }
+
+}
+
+
+const otp=12345678;
+const outPut=validOtp(otp);
+console.log(outPut);
