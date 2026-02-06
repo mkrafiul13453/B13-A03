ed732e90f0d078e78081e428dfd7c338a498a6a9
mkrafiul13453 <mkrafiul13453@gamil.com>
2026-02-07 01:14:12 +0600

    Analyzer text is done

--- problem-5.js
+++ problem-5.js
@@ -1,4 +1,25 @@
 function  analyzeText(str) {
+    if(typeof str!=="string"|| str.trim()===""){
+        return "Invalid";
+    }
+
+    const theLine=str.split(" ").join("");
+    const length=theLine.length;
+
+    const lineArray =str.split(" ");
+    let longestWord="";
+    let big=0;
+
+    for(let word of lineArray){
+        if(word.length>big){
+            big=word.length;
+            longestWord=word;
+        }
+    }
+    return{
+         longwords:longestWord,
+        token:length,
+    };
 }
 const str="Programming is fun";
 const outPut=analyzeText(str);
